c8d53a2d7b8c0cf009bdc5b6afcda65bbf69dbb5
Wastalas <wastalasdassise@gmail.com>
2025-04-26 01:17:26 +0000

    files updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='llm_student_errors',
+    version='0.0.1',
+    author='motognon wastalas dogbalou',
+    author_email='wastalasdassise@gmail.com',
+    install_requires=[''],
+    packages=find_packages()
+
+)
